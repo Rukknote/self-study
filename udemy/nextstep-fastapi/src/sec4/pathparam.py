48ea59491bb0b205d902d696ccc9836b961c3df6
Rukknote <rukk.note@gmail.com>
2025-01-11 14:34:47 +0900

    merge udemy fastapi done

--- udemy/nextstep-fastapi/src/sec4/pathparam.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/countries/{country_name}")
-async def country(country_name: str): # 型を指定することで受け取るデータを制御できる
-    return {"country_name": country_name}
